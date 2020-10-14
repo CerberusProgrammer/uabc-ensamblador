5aaaf5ae8a17abc57a17ffdd9e4e78cf3bae8c53
Cerberus <cerberusprogrammer@gmail.com>
2020-10-14 13:48:40 -0700

    first commit

---
+++ practica2/hello.asm
@@ -0,0 +1,15 @@
+section .data
+msg db "Hola mundo", 0 ;Mensaje
+    len equ $ - msg ;Tamaño del mensaje
+    section .text
+    global _start   ;Necesitamos exportar '_start' a ld, para que lo reconozca
+                    ;como el punto de entrada del programa.
+_start:
+    ;Escribimos el saludo a stdout.
+    mov eax, 4 ;Número de la llamada al sistema (sys_write).
+    mov ebx, 1 ;Primer argumento: file handle (stdout).
+    mov ecx, msg ;Segundo argumento: puntero a la cadena a escribir.
+    mov edx, len ;Tercer argumento: tamaño de la cadena.
+    int 80 ;Llama al Kernel.
+    mov eax, 1 ;Número de la llamada al sistema (sys_exit).
+    int 80 ;Llama al Kerne
